bcd0f3a4753ffc9d72405f7fdeb3964728051604
Fenixsinedbr <142617009+Fenixsinedbr@users.noreply.github.com>
2023-10-05 19:44:00 +0000

    Desenvolvimento

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
-// Lógica de Programação
-// Problema: obter 3 notas de um aluno, calcular a média,
-// exibir a média e exibir a situação do aluno: aprovado,
-// em exame ou reprovado.
+function mediaAluno() {
 
-console.log('Início do programa');
-
-// declaração das variáveis
-const nota1 = parseInt(prompt('Digite a nota 1:'));
-const nota2 = parseInt(prompt('Digite a nota 2:'));
-const nota3 = parseInt(prompt('Digite a nota 3:'));
-
-// cálculo da média
-const media = (nota1 + nota2 + nota3) / 3;
-
-console.log('A nota 1 é:', nota1);
-console.log('A nota 2 é:', nota2);
-console.log('A nota 3 é:', nota3);
-
-console.log('A média do aluno é:', media);
-
-console.log('Fim do programa');
+    // Lógica de programação
+    // Problema: receber 3 notas de um aluno,
+    // calcular e apresentar a média e o resultado
+    // se o aluno foi aprovado, reprovado ou ficou de exame.
+    console.log('Início do programa');
+    
+    //let nota1;
+    //let nota2;
+    //let nota3;
+    
+    //declaração das "variáveis"
+    const nota1 = parseInt(document.getElementById('nota1').value);
+    const nota2 = parseInt(document.getElementById('nota2').value);
+    const nota3 = parseInt(document.getElementById('nota3').value);
+    
+    //cálculo da média
+    media = (nota1 + nota2 + nota3) / 3;
+    
+    //console.log('A Nota 1 é:', nota1);
+    //console.log('A Nota 2 é:', nota2);
+    //console.log('A Nota 3 é:', nota3);  
+    
+    document.getElementById('resultado').innerHTML = 'A média do aluno é: ' + media + '<br>';
+    //console.log('A média do aluno é:', media);
+    
+    if (media >= 7) {
+       document.getElementById('resultado').innerHTML +=Situação' APROVADO';
+    } else if (media >= 5) {
+        document.getElementById('resultado').innerHTML += Situação' EXAME';
+    } else {
+        document.getElementById('resultado').innerHTML += Situação' REPROVADO';
+    }
+    
+    //if ( media < 7 && media >= 5) {
+    //   console.log('EXAME');
+    // else {
+    //   console.log('REPROVADO');
+    //}
+    
+    //if (media < 5 ) {
+    //    console.log('REPROVADO');
+    //}
+    
+    
+    console.log('Fim do programa ');
+    
+    }
